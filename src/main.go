9d50a6c2ba9e96b9057bd43741679884a595a0c3
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-05-16 15:11:09 -0700

    :hammer: chore: connect to slack app

--- src/main.go
+++ src/main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"context"
+	"github.com/amirhossein2831/goSlackBot/src/controller"
 	"github.com/joho/godotenv"
+	"github.com/shomali11/slacker"
 	"log"
 	"os"
 )
 
 func main() {
+	// load .env file
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal(err)
 	}
-	println(os.Getenv("SLACK_BOT_TOKEN"))
+
+	SBToken := os.Getenv("SLACK_BOT_TOKEN")
+	SAToken := os.Getenv("SLACK_APP_TOKEN")
+
+	// instantiate the bot
+	bot := slacker.NewClient(SBToken, SAToken)
+
+	// define the Commands below
+	controller.AgeCommand(bot)
+
+	// log the running commands
+	go controller.PrintCommandEvent(bot.CommandEvents())
+
+	// defer and close the context
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// start serve the bot
+	err = bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 }

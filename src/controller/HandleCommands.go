8b335ebad102145a0473b153da201e25503e3839
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-05-16 15:12:07 -0700

    :hammer: chore: handle age comand

--- src/controller/HandleCommands.go
+++ src/controller/HandleCommands.go
@@ -1,7 +1,10 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/shomali11/slacker"
+	"log"
+	"strconv"
 )
 
 func PrintCommandEvent(events <-chan *slacker.CommandEvent) {
@@ -12,4 +15,21 @@ func PrintCommandEvent(events <-chan *slacker.CommandEvent) {
 		println(event.Event)
 		println("***********************")
 	}
+}
+
+func HandleAgeCommand(botCtx slacker.BotContext, r slacker.Request, w slacker.ResponseWriter) {
+	year := r.Param("year")
+	yob, err := strconv.Atoi(year)
+	if err != nil {
+		log.Fatal(err)
+	}
+	age := 2024 - yob
+
+	res := fmt.Sprintf("so your age is %v", age)
+
+	err = w.Reply(res)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
